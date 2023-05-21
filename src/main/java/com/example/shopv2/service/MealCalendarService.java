24ae6b09d1f8f2a4d7f3ca07bbee5e3da7f49447
Krzysztof Sadej <kr.sadej@gmail.com>
2023-05-21 23:51:39 +0200

    Added LOGGER to MealCalendarService

--- src/main/java/com/example/shopv2/service/MealCalendarService.java
+++ src/main/java/com/example/shopv2/service/MealCalendarService.java
@@ -10,6 +10,8 @@ import com.example.shopv2.model.enums.MealTime;
 import com.example.shopv2.repository.MealCalendarRepository;
 import com.example.shopv2.validator.MealCalendarValidator;
 import com.example.shopv2.validator.enums.MealCalendarEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +26,8 @@ public class MealCalendarService {
     private final MealCalendarMapper mealCalendarMapper;
     private final MealCalendarValidator mealCalendarValidator;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MealCalendarService.class.getName());
+
     @Autowired
     public MealCalendarService(MealCalendarRepository mealCalendarRepository,
                                MealCalendarMapper mealCalendarMapper,
@@ -34,14 +38,18 @@ public class MealCalendarService {
     }
 
     public MealCalendar saveMealCalendar(MealCalendarRequest mealCalendarRequest){
+        LOGGER.info("Saved MealCalendarRequest by MealCalendarService");
+        LOGGER.debug("MealCalendarRequest: "+mealCalendarRequest);
         mealCalendarValidator.saveMealCalendarValidator(mealCalendarRequest);
 
         MealCalendar mealCalendar = mealCalendarMapper.requestToEntity(mealCalendarRequest);
+        LOGGER.debug("Data saved: "+mealCalendar);
         return mealCalendarRepository.save(mealCalendar);
     }
 
     public List<MealCalendarResponse> getByDayAndTime(Days day, MealTime time){
-
+        LOGGER.info("Method getByDayAndTime");
+        LOGGER.debug("Days: "+day+" Meal time: "+time);
         MealCalendarResponse mealCalendarResponse = new MealCalendarResponse(day, time);
         mealCalendarValidator.getByDayAndTimeValidator(mealCalendarResponse);
         List<MealCalendar> mealCalendars = mealCalendarRepository.findByDayAndTime(day, time);
@@ -51,11 +59,12 @@ public class MealCalendarService {
         for(int i=0; i<mealCalendars.size(); i++) {
             calendarResponses.add(mealCalendarMapper.entityToResponse(mealCalendars, i)) ;
         }
+        LOGGER.debug("Response from getByDayAndTime: "+calendarResponses);
         return calendarResponses;
     }
 
     public List<MealCalendarResponse> getCalendar(){
-
+        LOGGER.info("Method getCalendar");
         List<MealCalendar> mealCalendars = mealCalendarRepository.findAll();
 
         List<MealCalendarResponse> calendarResponses = new ArrayList<>();
@@ -64,11 +73,13 @@ public class MealCalendarService {
             calendarResponses.add(mealCalendarMapper.entityToResponse(mealCalendars, i));
         }
 
+        LOGGER.debug("Calendar returned: "+calendarResponses);
         return calendarResponses;
     }
 
     public void deleteMealCalendar(Long id) {
-
+        LOGGER.info("Method deleteMealCalendar");
+        LOGGER.info("Deleted element with id: "+id);
         mealCalendarValidator.deleteMealCalendarValidator(id);
         mealCalendarRepository.deleteById(id);
     }
